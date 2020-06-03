5982a70acec0badf6318f43cb79370440eae12b9
Eryck Kazeker <eryck_kazeker@hotmail.com>
2020-06-02 23:17:40 -0300

    Added barcode scanner POC

--- despensa/lib/scanner.dart
+++ despensa/lib/scanner.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
 class Scanner extends StatefulWidget {
   Scanner();
@@ -7,13 +9,47 @@ class Scanner extends StatefulWidget {
 }
 
 class ScannerState extends State<Scanner> {
+  var _scannedCode = '';
+  bool _scanned = false;
   ScannerState();
+
   @override
   Widget build(BuildContext context) {
+    
+    if(!this._scanned) {
+      barcodeScan();
+    }
+
     return Container(
       child: Center(
-        child: Text('Scanner')
+        child: Column(
+          children: <Widget>[
+            Text('Scanner'),
+            Text(_scannedCode)
+          ],
+        )
       ),
     );
+  }
+
+  Future barcodeScan() async {
+    try {
+      var barcode = await BarcodeScanner.scan();
+      setState(() {
+        this._scannedCode = barcode.rawContent;
+        this._scanned = true;
+      });
+    } on PlatformException catch(e) {
+      if (e.code == BarcodeScanner.cameraAccessDenied) {
+        setState(() => this._scannedCode = 'No camera Permission');
+      }
+      else {
+        setState(() => this._scannedCode = 'Unknown error $e');
+      }
+    } on FormatException {
+      setState(() => this._scannedCode = 'Nothing scanned');
+    } catch(e) {
+      setState(() => this._scannedCode = 'Unknown error $e');
+    }
   }
 }
